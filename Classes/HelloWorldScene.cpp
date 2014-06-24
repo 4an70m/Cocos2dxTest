fd87f89814f86aba700208093425284ae9d38e7e
mrpsihopath <mrpsihopath@gmail.com>
2014-06-24 11:51:36 +0300

    Added position methods: move(to/by), bezier(to/by), jump(to/by), place. Tutorial 5-12

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -36,6 +36,55 @@ bool HelloWorld::init()
     
     this->addChild(mySprite);
 
+    //positioning functions
+    //uncomment to try
+
+    //MoveBy positions a sprite, where x and y are absolute coordinates
+    //auto action1 = MoveBy::create(3, Point(100, 0));
+
+    //mySprite->runAction(action1);
+
+    //MoveTo position sprite, where x and y are relative coordinates
+    //auto action2 = MoveTo::create(3, Point(100,300));
+
+    //mySprite->runAction(action2);
+
+    //JumpBy position sprite, relative finishing position
+    //auto action3 = JumpBy::create(3, Point(100, 0), 50, 3);
+
+    //mySprite->runAction(action3);
+
+    //JumpTo position sprite, absolute finishing position
+    //auto action4 = JumpTo::create(3, Point(100, 30), 50, 6);
+
+	//mySprite->runAction(action4);
+
+    //moves, using bezier curves (кривые безье)
+    //ccBezierConfig bezier;
+    //bezier.controlPoint_1 = Point(0, visibleSize.height / 2);
+    //bezier.controlPoint_2 = Point(300, -visibleSize.height / 2);
+    //relative to the finishing point
+    //auto action5 = BezierBy::create(3, bezier);
+
+	//mySprite->runAction(action5);
+
+	//moves, using bezier curves (кривые безье)
+	//ccBezierConfig bezier;
+	//bezier.controlPoint_1 = Point(0, visibleSize.height / 2);
+	//bezier.controlPoint_2 = Point(300, -visibleSize.height / 2);
+	//bezier.endPosition = Point(200,100);
+	//absolute to the finishing point
+	//auto action6 = BezierTo::create(8, bezier);
+
+	//mySprite->runAction(action6);
+
+    //places sprite by absolute coordinates
+	//auto action7 = Place::create(Point(98, 289));
+
+	//mySprite->runAction(action7);
+
+
+
     return true;
 }
 
